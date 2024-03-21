6c8ed0bd244a28145e47495126021c8e3ae6b9d0
StellairDEV-Officiel <zacharie.roger@outlook.com>
2024-03-21 15:07:55 +0100

    repair routes

--- start/routes.ts
+++ start/routes.ts
@@ -36,91 +36,91 @@ router.group(() => {
 }).prefix('auth')
 
 router.group(() => {
-  router.get('invoices', [InvoicesController, 'index']).middleware('auth')
-  router.get('invoices/:id', [InvoicesController, 'show']).middleware('auth')
-  router.post('invoices', [InvoicesController, 'store']).middleware('auth')
-  router.put('invoices/:id', [InvoicesController, 'update']).middleware('auth')
-  router.delete('invoices/:id', [InvoicesController, 'destroy']).middleware('auth')
-}).prefix('invoices')
+  router.get('invoices', [InvoicesController, 'index'])
+  router.get('invoices/:id', [InvoicesController, 'show'])
+  router.post('invoices', [InvoicesController, 'store'])
+  router.put('invoices/:id', [InvoicesController, 'update'])
+  router.delete('invoices/:id', [InvoicesController, 'destroy'])
+}).prefix('invoices').use(middleware.auth())
 
 router.group(() => {
-  router.get('invoice-items', [InvoiceItemsController, 'index']).middleware('auth')
-  router.get('invoice-items/:id', [InvoiceItemsController, 'show']).middleware('auth')
-  router.post('invoice-items', [InvoiceItemsController, 'store']).middleware('auth')
-  router.put('invoice-items/:id', [InvoiceItemsController, 'update']).middleware('auth')
-  router.delete('invoice-items/:id', [InvoiceItemsController, 'destroy']).middleware('auth')
-}).prefix('invoices-items')
+  router.get('invoice-items', [InvoiceItemsController, 'index'])
+  router.get('invoice-items/:id', [InvoiceItemsController, 'show'])
+  router.post('invoice-items', [InvoiceItemsController, 'store'])
+  router.put('invoice-items/:id', [InvoiceItemsController, 'update'])
+  router.delete('invoice-items/:id', [InvoiceItemsController, 'destroy'])
+}).prefix('invoices-items').use(middleware.auth())
 
 router.group(() => {
-  router.get('messages', [MessagesController, 'index']).middleware('auth');
-  router.get('messages/:id', [MessagesController, 'show']).middleware('auth');
-  router.post('messages', [MessagesController, 'store']).middleware('auth');
-  router.put('messages/:id', [MessagesController, 'update']).middleware('auth');
-  router.delete('messages/:id', [MessagesController, 'destroy']).middleware('auth');
-}).prefix('messages');
+  router.get('messages', [MessagesController, 'index'])
+  router.get('messages/:id', [MessagesController, 'show'])
+  router.post('messages', [MessagesController, 'store'])
+  router.put('messages/:id', [MessagesController, 'update'])
+  router.delete('messages/:id', [MessagesController, 'destroy'])
+}).prefix('messages').use(middleware.auth())
 
 router.group(() => {
-  router.get('owner-plannings', [OwnerPlanningsController, 'index']).middleware('auth');
-  router.get('owner-plannings/:id', [OwnerPlanningsController, 'show']).middleware('auth');
-  router.post('owner-plannings', [OwnerPlanningsController, 'store']).middleware('auth');
-  router.put('owner-plannings/:id', [OwnerPlanningsController, 'update']).middleware('auth');
-  router.delete('owner-plannings/:id', [OwnerPlanningsController, 'destroy']).middleware('auth');
-}).prefix('owner-plannings');
+  router.get('owner-plannings', [OwnerPlanningsController, 'index'])
+  router.get('owner-plannings/:id', [OwnerPlanningsController, 'show'])
+  router.post('owner-plannings', [OwnerPlanningsController, 'store'])
+  router.put('owner-plannings/:id', [OwnerPlanningsController, 'update'])
+  router.delete('owner-plannings/:id', [OwnerPlanningsController, 'destroy'])
+}).prefix('owner-plannings').use(middleware.auth())
 
 router.group(() => {
-  router.get('properties', [PropertiesController, 'index']).middleware('auth');
-  router.get('properties/:id', [PropertiesController, 'show']).middleware('auth');
-  router.post('properties', [PropertiesController, 'store']).middleware('auth');
-  router.put('properties/:id', [PropertiesController, 'update']).middleware('auth');
-  router.delete('properties/:id', [PropertiesController, 'destroy']).middleware('auth');
-}).prefix('properties');
+  router.get('properties', [PropertiesController, 'index'])
+  router.get('properties/:id', [PropertiesController, 'show'])
+  router.post('properties', [PropertiesController, 'store'])
+  router.put('properties/:id', [PropertiesController, 'update'])
+  router.delete('properties/:id', [PropertiesController, 'destroy'])
+}).prefix('properties').use(middleware.auth())
 
 router.group(() => {
-  router.get('property-images', [PropertyImagesController, 'index']).middleware('auth');
-  router.get('property-images/:id', [PropertyImagesController, 'show']).middleware('auth');
-  router.post('property-images', [PropertyImagesController, 'store']).middleware('auth');
-  router.put('property-images/:id', [PropertyImagesController, 'update']).middleware('auth');
-  router.delete('property-images/:id', [PropertyImagesController, 'destroy']).middleware('auth');
-}).prefix('property-images');
+  router.get('property-images', [PropertyImagesController, 'index'])
+  router.get('property-images/:id', [PropertyImagesController, 'show'])
+  router.post('property-images', [PropertyImagesController, 'store'])
+  router.put('property-images/:id', [PropertyImagesController, 'update'])
+  router.delete('property-images/:id', [PropertyImagesController, 'destroy'])
+}).prefix('property-images').use(middleware.auth())
 
 router.group(() => {
-  router.get('provider-plannings', [ProviderPlanningsController, 'index']).middleware('auth');
-  router.get('provider-plannings/:id', [ProviderPlanningsController, 'show']).middleware('auth');
-  router.post('provider-plannings', [ProviderPlanningsController, 'store']).middleware('auth');
-  router.put('provider-plannings/:id', [ProviderPlanningsController, 'update']).middleware('auth');
-  router.delete('provider-plannings/:id', [ProviderPlanningsController, 'destroy']).middleware('auth');
-}).prefix('provider-plannings');
+  router.get('provider-plannings', [ProviderPlanningsController, 'index'])
+  router.get('provider-plannings/:id', [ProviderPlanningsController, 'show'])
+  router.post('provider-plannings', [ProviderPlanningsController, 'store'])
+  router.put('provider-plannings/:id', [ProviderPlanningsController, 'update'])
+  router.delete('provider-plannings/:id', [ProviderPlanningsController, 'destroy'])
+}).prefix('provider-plannings').use(middleware.auth())
 
 router.group(() => {
-  router.get('provider-services', [ProviderServicesController, 'index']).middleware('auth');
-  router.get('provider-services/:id', [ProviderServicesController, 'show']).middleware('auth');
-  router.post('provider-services', [ProviderServicesController, 'store']).middleware('auth');
-  router.put('provider-services/:id', [ProviderServicesController, 'update']).middleware('auth');
-  router.delete('provider-services/:id', [ProviderServicesController, 'destroy']).middleware('auth');
-}).prefix('provider-services');
+  router.get('provider-services', [ProviderServicesController, 'index'])
+  router.get('provider-services/:id', [ProviderServicesController, 'show'])
+  router.post('provider-services', [ProviderServicesController, 'store'])
+  router.put('provider-services/:id', [ProviderServicesController, 'update'])
+  router.delete('provider-services/:id', [ProviderServicesController, 'destroy'])
+}).prefix('provider-services').use(middleware.auth())
 
 router.group(() => {
-  router.get('reviews', [ReviewsController, 'index']).middleware('auth');
-  router.get('reviews/:id', [ReviewsController, 'show']).middleware('auth');
-  router.post('reviews', [ReviewsController, 'store']).middleware('auth');
-  router.put('reviews/:id', [ReviewsController, 'update']).middleware('auth');
-  router.delete('reviews/:id', [ReviewsController, 'destroy']).middleware('auth');
-}).prefix('reviews');
+  router.get('reviews', [ReviewsController, 'index'])
+  router.get('reviews/:id', [ReviewsController, 'show'])
+  router.post('reviews', [ReviewsController, 'store'])
+  router.put('reviews/:id', [ReviewsController, 'update'])
+  router.delete('reviews/:id', [ReviewsController, 'destroy'])
+}).prefix('reviews').use(middleware.auth())
 
 router.group(() => {
-  router.get('societies', [SocietiesController, 'index']).middleware('auth');
-  router.get('societies/:id', [SocietiesController, 'show']).middleware('auth');
-  router.post('societies', [SocietiesController, 'store']).middleware('auth');
-  router.put('societies/:id', [SocietiesController, 'update']).middleware('auth');
-  router.delete('societies/:id', [SocietiesController, 'destroy']).middleware('auth');
-}).prefix('societies');
+  router.get('societies', [SocietiesController, 'index'])
+  router.get('societies/:id', [SocietiesController, 'show'])
+  router.post('societies', [SocietiesController, 'store'])
+  router.put('societies/:id', [SocietiesController, 'update'])
+  router.delete('societies/:id', [SocietiesController, 'destroy'])
+}).prefix('societies').use(middleware.auth())
 
 router.group(() => {
-  router.get('transactions', [TransactionsController, 'index']).middleware('auth');
-  router.get('transactions/:id', [TransactionsController, 'show']).middleware('auth');
-  router.post('transactions', [TransactionsController, 'store']).middleware('auth');
-  router.put('transactions/:id', [TransactionsController, 'update']).middleware('auth');
-  router.delete('transactions/:id', [TransactionsController, 'destroy']).middleware('auth');
-}).prefix('transactions');
+  router.get('transactions', [TransactionsController, 'index'])
+  router.get('transactions/:id', [TransactionsController, 'show'])
+  router.post('transactions', [TransactionsController, 'store'])
+  router.put('transactions/:id', [TransactionsController, 'update'])
+  router.delete('transactions/:id', [TransactionsController, 'destroy'])
+}).prefix('transactions').use(middleware.auth())
 
 export default router;
